bb329d13955e0c73fecc100062b719910595fa58
Mingliang-Xu <oliver.xml@hotmail.com>
2023-09-13 11:28:26 -0700

    initial commit GOT

---
+++ varsAndConditionals.js
@@ -0,0 +1,14 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+let johnSnowAttack = 35;
+let jamieLannisterAttack = 45;
+if(johnSnowAttack>jamieLannisterAttack){
+    console,console.log("johnSnowAttack is beter than jamieLannisterAttack");
+}else if(jamieLannisterAttack>johnSnowAttack){
+    console.log("jamieLannisterAttack is better than johnSnowAttack");
+}else{
+    console.log("jamieLannisterAttack is same as johnSnowAttack");
+}
