541a47063b71609bdeeffb188745f7228fec7cc5
Mingliang-Xu <oliver.xml@hotmail.com>
2023-09-13 11:31:55 -0700

    string result wording changed

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -6,9 +6,9 @@
 let johnSnowAttack = 35;
 let jamieLannisterAttack = 45;
 if(johnSnowAttack>jamieLannisterAttack){
-    console,console.log("johnSnowAttack is beter than jamieLannisterAttack");
+    console,console.log("johnSnowAttack has beter attack than jamieLannisterAttack");
 }else if(jamieLannisterAttack>johnSnowAttack){
-    console.log("jamieLannisterAttack is better than johnSnowAttack");
+    console.log("jamieLannisterAttack has better attack than johnSnowAttack");
 }else{
-    console.log("jamieLannisterAttack is same as johnSnowAttack");
+    console.log("jamieLannisterAttack has same attack as johnSnowAttack");
 }
