52b439e811ed86eda16883eeeb6335f90b3e5568
Mingliang-Xu <oliver.xml@hotmail.com>
2023-09-13 12:06:45 -0700

    Add JohnSnow Health

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -3,12 +3,22 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
-let johnSnowAttack = 35;
-let jamieLannisterAttack = 45;
+let johnSnowAttack = 25;
+let jamieLannisterAttack = 35;
 if(johnSnowAttack>jamieLannisterAttack){
     console,console.log("johnSnowAttack has beter attack than jamieLannisterAttack");
 }else if(jamieLannisterAttack>johnSnowAttack){
     console.log("jamieLannisterAttack has better attack than johnSnowAttack");
 }else{
     console.log("jamieLannisterAttack has same attack as johnSnowAttack");
+}
+
+let johnSnowHealth = 100;
+let johnSnowDefense = 0;
+
+if(johnSnowHealth<=jamieLannisterAttack){
+    console.log("JS has been slayed");
+}else{
+    johnSnowHealth = johnSnowHealth-jamieLannisterAttack
+    console.log(`JS health is down to ${johnSnowHealth}`)
 }
